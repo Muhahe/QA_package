93ec0bf776a94a4d6abe052e8def4f495c270017
Luboš Suk <lubos.suk@warhorsestudios.cz>
2017-11-13 17:09:20 +0100

    Přidejte soubory projektu.

---
+++ XBOXValidator/HelperClasses/folderPicker(1).cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using XBOXValidator.Classes;
+using XBOXValidator.Model;
+
+namespace XBOXValidator.classes
+{
+    class FolderPicker
+    {
+        private ICommand _pickFolderCommand;
+        private FolderBrowserDialog openFolder = new FolderBrowserDialog();
+        private FolderPathModel folderPathModel;
+
+        public FolderPicker()
+        {
+            folderPathModel = new FolderPathModel();
+        }
+
+        public ICommand PickFolderCommand
+        {
+
+            get
+            {
+                if (_pickFolderCommand == null)
+                {
+                    _pickFolderCommand = new RelayCommand(
+                        param => this.CanPickFolder(),
+                        param => this.PickFolder()
+                        );
+
+                }
+                return _pickFolderCommand;
+            }
+
+        }
+
+        public FolderPathModel FolderPathModel
+        {
+            get
+            {
+                return folderPathModel;
+            }
+            set
+            {
+                folderPathModel = value;
+            }
+        }
+
+
+        private void PickFolder()
+        {
+            FolderBrowserDialog folderPicker = new FolderBrowserDialog();
+            openFolder.SelectedPath = "C:\\";
+            DialogResult result = openFolder.ShowDialog();
+            if (result.ToString() == "OK")
+            {
+                folderPathModel.FolderPath = openFolder.SelectedPath;
+            }
+
+        }
+
+        private bool CanPickFolder()
+        {
+            return true;
+        }
+
+
+    }
+}
---
+++ XBOXValidator/Model/SingleSnapshotModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XBOXValidator.HelperClasses;
+
+namespace XBOXValidator.Model
+{
+    class SingleSnapshotModel
+    {
+        public String SnapshotPath { get; }
+        public String EntityNumber { get; }
+        public String ErrorNumber { get; }
+        public String SnapshotId { get; }
+        public String EntityName { get; }
+        public String ErrorNode { get; }
+        public String ErrorMessage {get;}
+        public bool Included { get; set; }
+
+        public SingleSnapshotModel(String path) {
+            this.SnapshotPath = path;
+            this.Included = true;
+            String[] splitedPath = path.Split(new string[] { Path.DirectorySeparatorChar.ToString() }, StringSplitOptions.None);
+            int pathCount = splitedPath.Count();
+
+            this.EntityName = splitedPath[pathCount - 2];
+            this.SnapshotId = splitedPath[pathCount - 3];
+            this.EntityNumber = this.SnapshotId.Split('_').First();
+            this.ErrorNumber = this.SnapshotId.Split('_').Last();
+            XmlFileReader xmlFileReader = new XmlFileReader(path);
+
+            this.ErrorMessage = xmlFileReader.GetErrorNodeText();
+        }
+
+        private void SetEntityNumber() {
+
+        }
+
+        
+
+
+
+
+    }
+}
